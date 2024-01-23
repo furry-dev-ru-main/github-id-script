830b757386da76bbd65fdda6d581724ec539f5bd
phoenix <85183285+PhoenixOrigin@users.noreply.github.com>
2024-01-23 21:17:56 +0000

    help

--- index.js
+++ index.js
@@ -1,4 +1,25 @@
-const core = require('@actions/core');
-const github = require('@actions/github');
+const core = await import('@actions/core');
+const github = await import('@actions/github');
+const fs = await import('node:fs');
 
-// will finish later
+const files = core.getInput('files').split(" ");
+
+for(const file in files) {
+    if(!file.startsWith("./domains")) continue;
+    const data = fs.readFileSync(file, 'utf8');
+    let json = JSON.parse(data);
+
+    if(json.owner.hasOwnProperty("github_id")) {
+        console.log("Skipping " + file + " as github_id is already present");
+        continue;
+    }
+
+    const username = json.owner.username;
+    const api = await fetch("https://api.github.com/users/" + username);
+    const api_json = await api.json();
+    json.owner.github_id = api_json.id;
+
+    await fs.writeFileSync(file, JSON.stringify(json, null, 2));
+
+    console.log("Updating " + file + " with github_id as " + api_json.id);
+}
