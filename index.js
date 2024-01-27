5c1a74410c0956475a746964c45ab78525924fed
phoenix <85183285+PhoenixOrigin@users.noreply.github.com>
2024-01-27 11:33:56 +0000

    Asynchronous code + API verification

--- index.js
+++ index.js
@@ -3,22 +3,28 @@ const github = await import('@actions/github');
 const fs = await import('node:fs');
 
 const files = core.getInput('files').split(" ");
-for(const file of files) {
-    if(!file.startsWith("domains")) continue;
-    const data = fs.readFileSync(file, 'utf8');
-    let json = JSON.parse(data);
+const token = core.getInput("token");
 
-    const username = json.owner.username;
-    if(username === "is-a-dev") continue;
+files.forEach(async(file) => {
+        if(!file.startsWith("domains")) return;
+        const data = fs.readFileSync(file, 'utf8');
+        let json = JSON.parse(data);
 
-    const api = await fetch("https://api.github.com/users/" + username);
-    const api_json = await api.json();
-    if(!api_json.hasOwnProperty("id")) {
-        continue;
-    }
-    json.owner.github_id = api_json.id;
+        const username = json.owner.username;
+        if(username === "is-a-dev") return;
 
-    await fs.writeFileSync(file, JSON.stringify(json, null, 2));
+        const api = await fetch("https://api.github.com/users/" + username, {
+            headers: {
+                "Authorization": "Bearer " + token
+            }
+        });
+        const api_json = await api.json();
+        if(!api_json.hasOwnProperty("id")) {
+            return;
+        }
+        json.owner.github_id = api_json.id;
 
-    console.log("Updating " + file + " with github_id as " + api_json.id);
-}
+        await fs.writeFileSync(file, JSON.stringify(json, null, 2));
+
+        console.log("Updating " + file + " with github_id as " + api_json.id);
+});
